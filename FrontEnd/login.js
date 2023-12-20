10951ef92cdb084eca268d1bcd809dcc6e49cfeb
IbLeDev <ibrahim8.fofana@gmail.com>
2023-12-20 05:07:00 +0100

    add login semi fonctionnel

---
+++ FrontEnd/login.js
@@ -0,0 +1,23 @@
+function login() {
+  const form = document.querySelector("#form-login");
+
+  form.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    const email = event.target.querySelector("[name=e-mail]").value;
+    const motDePasse = event.target.querySelector("[name=mot-de-passe]").value;
+    const body = {
+      email: email,
+      password: motDePasse,
+    };
+    const reponse = await fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+    const reponseJson = await reponse.json();
+    const token = reponseJson.token;
+    window.localStorage.setItem("token", token);
+  });
+}
+
+login();
