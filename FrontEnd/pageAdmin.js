65170cf36462ec52b4b81b9bb36fcaf585e2d625
IbLeDev <ibrahim8.fofana@gmail.com>
2023-12-24 12:40:48 +0100

    add début style bouton modification

--- FrontEnd/pageAdmin.js
+++ FrontEnd/pageAdmin.js
@@ -2,7 +2,7 @@ import { afficherModale } from "./afficherModale.js";
 
 export function pageAdmin() {
   let boutonModifier = document.createElement("button");
-  boutonModifier.setAttribute("id", "boutonModier");
+  boutonModifier.setAttribute("id", "bouton--modification");
   boutonModifier.innerHTML = "Modifier";
   let portfolio = document.querySelector("#portfolio");
   portfolio.appendChild(boutonModifier);
