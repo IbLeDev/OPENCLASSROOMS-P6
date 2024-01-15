557dd9eadf6b49b276a78aef5082145998a10aea
IbLeDev <ibrahim8.fofana@gmail.com>
2024-01-15 14:23:14 +0100

    add icone page admin button modifier

--- FrontEnd/pageAdmin.js
+++ FrontEnd/pageAdmin.js
@@ -5,9 +5,19 @@ export function pageAdmin() {
   let boutonModifier = document.createElement("button");
   boutonModifier.setAttribute("id", "bouton--modification");
   boutonModifier.innerHTML = "Modifier";
+
+  let iconeModifier = document.createElement("i");
+  iconeModifier.classList.add("fa-regular");
+  iconeModifier.classList.add("fa-pen-to-square");
+  boutonModifier.appendChild(iconeModifier);
+
+  let buttons = document.querySelector(".buttons");
+  buttons.remove();
+
   let portfolio = document.querySelector("#portfolio");
   portfolio.appendChild(boutonModifier);
   boutonModifier.addEventListener("click", afficherModale);
+
   let out = document.querySelector("nav a");
   out.innerHTML = "logout";
   out.setAttribute("href", "#");
